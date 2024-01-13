c77b3ed67dcf58ddce10728beb154709e2d6adca
gastonduault <gas.duault@gmail.com>
2024-01-13 12:44:54 +0100

    class Parser >>  add player, get player, json to csv

--- src/Parser.py
+++ src/Parser.py
@@ -1,26 +1,38 @@
-import json
+import csv
 
-path_file = "data.json"
+path_file = "data.csv"
 
 class Parser:
+    player = []
     def addPlayer(self, name):
-        with open(path_file, 'w') as file:
-            json.dump(name, file)
+        from Player import Player
+        self.getPlayers()
+        if self.playerExist(name) is None:
+            self.player.append(Player(name, self.lastIdPlayer()+1))
+            with open(path_file, 'w') as file:
+                fieldnames = ["name", "id"]
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
+                for p in self.player:
+                    writer.writerow(p.to_dict())
 
     def getPlayers(self):
         from Player import Player
-        tableOfPlayer = []
+        self.player = []
         with open(path_file, 'r') as file:
-            jsonData = json.load(file)
-            for jsonObj in jsonData:
-                tableOfPlayer.append(Player(jsonObj["name"] ,jsonObj["id"]));
-        return tableOfPlayer
+            reader = csv.DictReader(file)
+            for row in reader:
+                p = Player(row["name"], int(row["id"]))
+                self.player.append(p)
+        return self.player
 
     def playerExist(self, name):
-        for player in self.getPlayers():
-            if player.name == name:
-                return True
-        return False
+        for p in self.player:
+            if p.name == name:
+                return p.id
+        return None
 
     def lastIdPlayer(self):
-        return self.getPlayers()[-1].id
+        if len(self.player) > 0:
+            return self.player[-1].id
+        return 0
