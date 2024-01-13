df6285230b67cb105d05039be1e832db3121bb3d
Vladouh <guillemontvladimir@gmail.com>
2024-01-13 12:13:38 +0100

    Mise à jour des fonctions de Score

--- src/Score.py
+++ src/Score.py
@@ -4,19 +4,39 @@ class Score:
         # 1 tuple correspond à 1 lancé
         self.score = [None] * 10
 
-    def ajouterScore(self, numeroLancer, lancer1, lancer2=0):
-        if(0 <= numeroLancer < 10):
+    def ajouterScore(self, numeroLancer, lancer1=0, lancer2=0):
+        if(0 <= numeroLancer < 10) & (lancer1 + lancer2 <= 10):
             self.score[numeroLancer] = (lancer1, lancer2)
         else:
-            print("Numéro de lancer invalide")
+            print(f"\nLancer invalide : n°{numeroLancer}, score {lancer1+lancer2}")
 
-    def afficherScore(self):
-        print("Scores :")
+    def afficherLancer(self):
+        print("\n--- Affichage des Lancer ---")
         for i, score_tuple in enumerate(self.score):
             if score_tuple is not None:
                 print(f"Lancer n°{i + 1} = {self.score[i]}")
 
+    def Spare(self, numeroLancer):
+        if(self.score[numeroLancer][0] + self.score[numeroLancer][1] == 10):
+            return True
+
+    def Strike(self, numeroLancer):
+        if(self.score[numeroLancer][0] == 10):
+            return True
+
     def calculLancer(self, numeroLancer):
+        if(numeroLancer == 0):
+            somme = self.score[numeroLancer][0] + self.score[numeroLancer][1]
+            return somme
+
+        if(self.Strike(numeroLancer - 1)):
+            somme = 2 * self.score[numeroLancer][0] + 2 * self.score[numeroLancer][1]
+            return somme
+
+        if(self.Spare(numeroLancer - 1)):
+            somme = 2 * self.score[numeroLancer][0] + self.score[numeroLancer][1]
+            return somme
+
         somme = self.score[numeroLancer][0] + self.score[numeroLancer][1]
         return somme
 
--- tests/test_score.py
+++ tests/test_score.py
@@ -8,16 +8,29 @@ def test_incrementation_score():
     Score2.ajouterScore(0,3,4)
     Score2.ajouterScore(1, 7, 2)
     Score2.ajouterScore(2, 1, 0)
+    Score2.ajouterScore(11, 1, 7)
+    Score2.ajouterScore(7, 14, 7)
 
 def test_affichage_score():
     Score3 = Score()
-    Score3.ajouterScore(0, 3, 4)
-    Score3.ajouterScore(1, 0, 0)
-    Score3.afficherScore()
+    for i in range(10):
+        Score3.ajouterScore(i)
+    Score3.afficherLancer()
 
 def test_calcul_lancer():
     Score4 = Score()
     Score4.ajouterScore(0, 3, 4)
-    Score4.calculLancer(0)
+    Score4.ajouterScore(1, 8, 1)
+    Score4.ajouterScore(2, 8, 2)
+    Score4.ajouterScore(3, 5, 2)
+    Score4.ajouterScore(4, 10, 0)
+    Score4.ajouterScore(5, 7, 2)
+    print("")
+    Score4.afficherLancer()
     assert(Score4.calculLancer(0) == 7)
+    assert(Score4.calculLancer(1) == 9)
+    assert(Score4.calculLancer(2) == 10)
+    assert(Score4.calculLancer(3) == 12)
+    assert(Score4.calculLancer(4) == 10)
+    assert(Score4.calculLancer(5) == 18)
 
