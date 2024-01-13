3825d66f86d2274f99e4ddac00b752dba507281d
julie <julegendre16@gmail.com>
2024-01-13 16:55:22 +0100

    update tests class player

--- tests/test_classPlayer.py
+++ tests/test_classPlayer.py
@@ -1,5 +1,9 @@
 from src.Player import Player
 
+def init_player():
+    player = Player("Alice")
+    assert player is not None
+    assert isinstance(player, Player)
 
 def test_get_name():
     player = Player("Alice")
@@ -8,6 +12,9 @@ def test_get_name():
 def test_id():
     player1 = Player("Alice")
     assert player1.get_id() == None
-    player2 = Player("Alice", 1)
+    player2 = Player("Mathilde", 1)
     assert player2.get_id() == 1
-    #tester deux id soient differents
+
+def test_to_dict():
+    player = Player("Alice", 1)
+    assert player.to_dict() == {"name": "Alice", "id": 1}
