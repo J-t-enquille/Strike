83c5fd18259b11fc6e71c0e6861f61fb4a9c10f4
julie <julegendre16@gmail.com>
2024-01-19 08:38:56 +0100

    update to fix PEP8

--- tests/test_score.py
+++ tests/test_score.py
@@ -1,5 +1,5 @@
 from src.Score import Score
-import pytest
+
 
 def test_creation_score():
     score = Score()
@@ -13,32 +13,29 @@ def test_creation_score():
     assert score2.score == [None] * 5
 
 
-
 def test_ajouterScore():
     score = Score()
-    score.ajouterScore(0,3,4)
+    score.ajouterScore(0, 3, 4)
     score.ajouterScore(1, 7, 2)
     score.ajouterScore(7, 14, 7)
-    assert score.score[0] == (3,4)
-    assert score.score[1] == (7,2)
-    assert score.score[7] == None
-    assert score.ajouterScore(11,1,7) == False
-    assert score.ajouterScore(7, 14, 7) == False
+    assert score.score[0] == (3, 4)
+    assert score.score[1] == (7, 2)
+    assert score.score[7] is None
+    assert score.ajouterScore(11, 1, 7) is False
+    assert score.ajouterScore(7, 14, 7) is False
 
-    score2 = Score(5,5)
-    score2.ajouterScore(0,1,3)
-    assert score2.score[0] == (1,3)
-    assert score2.ajouterScore(6,3,1) == False
-    assert score2.ajouterScore(2,5,4) == False
-
+    score2 = Score(5, 5)
+    score2.ajouterScore(0, 1, 3)
+    assert score2.score[0] == (1, 3)
+    assert score2.ajouterScore(6, 3, 1) is False
+    assert score2.ajouterScore(2, 5, 4) is False
 
 
 def test_affichage_score():
     score = Score()
     for i in range(10):
         score.ajouterScore(i)
-        assert score.score[i] == (0,0)
-
+        assert score.score[i] == (0, 0)
 
 
 def test_affichage_score2(capsys):
@@ -53,7 +50,6 @@ def test_affichage_score2(capsys):
     assert captured.out == expected_output
 
 
-
 def test_calculScoreLancer():
     score = Score(3)
     score.ajouterScore(0, 3, 4)
@@ -65,27 +61,27 @@ def test_calculScoreLancer():
     assert score.calculScoreLancer(2) == 11
 
     score2 = Score(3)
-    score2.ajouterScore(0,8,2)
-    score2.ajouterScore(1,7,2)
-    score2.ajouterScore(2,10,10,10)
+    score2.ajouterScore(0, 8, 2)
+    score2.ajouterScore(1, 7, 2)
+    score2.ajouterScore(2, 10, 10, 10)
 
     assert score2.calculScoreLancer(0) == 17
     assert score2.calculScoreLancer(1) == 9
     assert score2.calculScoreLancer(2) == 30
 
     score3 = Score(3)
-    score3.ajouterScore(0,10)
-    score3.ajouterScore(1,6,1)
-    score3.ajouterScore(2,3,5)
+    score3.ajouterScore(0, 10)
+    score3.ajouterScore(1, 6, 1)
+    score3.ajouterScore(2, 3, 5)
 
     assert score3.calculScoreLancer(0) == 17
     assert score3.calculScoreLancer(1) == 7
     assert score3.calculScoreLancer(2) == 8
 
     score4 = Score(3)
-    score4.ajouterScore(0,10)
-    score4.ajouterScore(1,10)
-    score4.ajouterScore(2,5,3)
+    score4.ajouterScore(0, 10)
+    score4.ajouterScore(1, 10)
+    score4.ajouterScore(2, 5, 3)
 
     assert score4.calculScoreLancer(0) == 25
     assert score4.calculScoreLancer(1) == 18
@@ -93,7 +89,7 @@ def test_calculScoreLancer():
 
     score5 = Score(3)
     score5.ajouterScore(0, 8, 2)
-    score5.ajouterScore(1, 7,3)
+    score5.ajouterScore(1, 7, 3)
     score5.ajouterScore(2, 5, 5, 5)
 
     assert score5.calculScoreLancer(0) == 17
@@ -108,7 +104,6 @@ def test_calculScoreLancer():
     assert score6.calculScoreLancer(0) == 20
     assert score6.calculScoreLancer(1) == 18
     assert score6.calculScoreLancer(2) == 8
-
 
 
 def test_score_total():
@@ -130,7 +125,6 @@ def test_score_total():
     assert scoreTotal2 == 50
 
 
-
 def test_tableauScoreCourant():
     score = Score(5)
     score.ajouterScore(0, 8, 2)
@@ -139,9 +133,8 @@ def test_tableauScoreCourant():
     score.ajouterScore(3, 10)
     score.ajouterScore(4, 5, 4)
 
-    assert score.tableauScoreCourant() == [20,38,46,65,74]
+    assert score.tableauScoreCourant() == [20, 38, 46, 65, 74]
     assert score.scoreTotal() == 74
-
 
 
 def test_calculScoreCourant():
